224537de4e2bd2cf988d031458d586a51ef5c1f0
Syndesi <soerenklein98@gmail.com>
2024-08-18 20:15:44 +0200

    Nuxt prototype.

---
+++ pwa/nuxt.config.ts
@@ -0,0 +1,26 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
---
+++ pwa/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <v-btn>
+      Button
+    </v-btn>
+  </div>
+</template>
