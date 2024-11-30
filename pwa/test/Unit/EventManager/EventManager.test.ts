8c4f6c739e97156be3f1cb5ceccf817d79d50f14
Syndesi <soerenklein98@gmail.com>
2024-11-30 19:01:41 +0100

    Wip

--- pwa/test/Unit/EventManager/EventManager.test.ts
+++ pwa/test/Unit/EventManager/EventManager.test.ts
@@ -1,6 +1,61 @@
 import { expect } from 'chai';
 import {EventManager} from "../../../src/EventManager/EventManager";
+import {Event} from "../../../src/Type/Definition/Event";
+import {EventIdentifier} from "../../../src/Type/Definition/EventIdentifier";
+import {validateEventIdentifierFromString} from "../../../src/Type/Definition/EventIdentifier";
+import {EventHandler} from "../../../src/Type/Definition/EventHandler";
+import {
+  EventHandlerIdentifier,
+  validateEventHandlerIdentifierFromString
+} from "../../../src/Type/Definition/EventHandlerIdentifier";
+import {EventIdentifier} from "@ember-nexus/web-sdk/Type/Enum";
+import {validatePluginIdFromString} from "../../../src/Type/Definition/PluginId";
 
+
+class TestEventA implements Event {
+  getEventIdentifier(): EventIdentifier {
+    return validateEventIdentifierFromString('test-event-a');
+  }
+}
+
+class TestEventB implements Event {
+  getEventIdentifier(): EventIdentifier {
+    return validateEventIdentifierFromString('test-event-b');
+  }
+}
+
+class TestEventHandler1 implements EventHandler {
+  getEventHandlerIdentifier(): EventHandlerIdentifier {
+    return validateEventHandlerIdentifierFromString(validatePluginIdFromString('test-plugin'), 'handler-1');
+  }
+
+  getHandleableEvents(): EventIdentifier[] {
+    return [
+      validateEventIdentifierFromString('test-event-a')
+    ];
+  }
+
+  handleEvent(_event: Event): void {
+  }
+
+}
+
+class TestEventHandler2 implements EventHandler {
+  getEventHandlerIdentifier(): EventHandlerIdentifier {
+    return validateEventHandlerIdentifierFromString(validatePluginIdFromString('test-plugin'), 'handler-2');
+  }
+
+  getHandleableEvents(): EventIdentifier[] {
+    return [
+      validateEventIdentifierFromString('test-event-a'),
+      validateEventIdentifierFromString('test-event-b'),
+    ];
+  }
+
+  handleEvent(_event: Event): void {
+  }
+
+}
 
 describe('EvenManager tests', () => {
 
@@ -10,5 +65,18 @@ describe('EvenManager tests', () => {
     expect(eventManager.getEventHandlers()).to.be.an( "array" ).that.is.empty;
     expect(eventManager.clearEventHandlers()).to.equal(eventManager);
   });
+
+  it('should be able to use it without problems', () => {
+    const eventManager = new EventManager();
+
+    const eventHandler1 = new TestEventHandler1();
+    eventManager.registerEventHandler(eventHandler1);
+    const eventHandler2 = new TestEventHandler2();
+    eventManager.registerEventHandler(eventHandler2);
+
+    
+
+  });
+
 
 });
