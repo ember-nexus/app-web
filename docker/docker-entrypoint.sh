71fea1d6f1fa0c8fab555b4398f715562d64f110
Syndesi <soerenklein98@protonmail.com>
2026-01-17 10:49:18 +0100

    wip

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+#todo: enable intentional container crash
+#if [ ! -f /well-known-security.txt ]; then
+#  echo "Error: Unable to start Ember Nexus API due to missing security.txt file at path /well-known-security.txt ."
+#  echo "See https://ember-nexus.github.io/api/ for details."
+#  exit 1
+#fi
+
+if [ -z "$@" ]; then
+  frankenphp run --config /etc/frankenphp/Caddyfile
+else
+  exec "$@"
+fi
